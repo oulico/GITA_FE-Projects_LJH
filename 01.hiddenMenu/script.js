c5065044b89a28c705e74192ec9594064de5b84e
manemis <manemis@gmail.com>
2022-05-30 02:46:06 +0900

    hiddenMenu

--- 01.hiddenMenu/script.js
+++ 01.hiddenMenu/script.js
@@ -1,1 +1,30 @@
-// 슬라이딩 다운
+function search() {
+  console.log("clicked");
+
+  const target = document.querySelector(".sns > li:last-child");
+  target.classList.toggle("on");
+}
+
+const gnb = document.querySelector("nav.gnb");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    console.log("we are going down");
+    gnb.classList.add("nav--hidden");
+  } else {
+    console.log("we are going up");
+    gnb.classList.remove("nav--hidden");
+  }
+
+  lastScrollY = window.scrollY;
+});
+
+function slideDown(n) {
+  console.log("slideDown");
+
+  const target = document.querySelector(`.cont__desc.${n}`);
+  console.log(`.cont__desc.${n}`);
+
+  target.classList.toggle(`on`);
+}
