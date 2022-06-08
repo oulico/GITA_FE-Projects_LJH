e7da9378521525a1b6c8f2d2a9a04dc3dabe1ed9
manemis <manemis@gmail.com>
2022-06-08 18:29:54 +0900

    now it can receive input value

--- 02.Co-Al-a/script.js
+++ 02.Co-Al-a/script.js
@@ -1,10 +1,29 @@
+let amountA;
+let amountB;
+let timeLast;
+let janPerSecA;
+let janPerSecB;
+
+const getValueInput = () => {
+  amountA = document.querySelector("#amountA").value;
+  amountB = document.querySelector("#amountB").value;
+  timeLast = document.querySelector("#timeLast").value;
+  console.log("amountA:" + amountA);
+  console.log("amountB:" + amountB);
+  console.log("timeLast:" + timeLast);
+  calculAlcPace(amountA, amountB, timeLast);
+};
+
 const calculAlcPace = (amountA, amountB, timeLast) => {
   let totalSec = timeLast * 3600;
   let totalJanA = amountA * 7;
   let totalJanB = amountB * 7;
-  let janPerSecA = parseInt(totalSec / totalJanA);
-  let janPerSecB = parseInt(totalSec / totalJanB);
+  janPerSecA = parseInt(totalSec / totalJanA);
+  janPerSecB = parseInt(totalSec / totalJanB);
   console.log(janPerSecA, janPerSecB);
 };
 
-calculAlcPace(2, 3, 1);
+///////// 로딩구역 //////////////
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("로딩완료");
+});
