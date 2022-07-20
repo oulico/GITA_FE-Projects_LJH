12ad12f40c7154aac626c60e031ec42555ddef06
manemis <manemis@gmail.com>
2022-07-20 16:39:10 +0900

    refactoring JS for Tickets page

--- 03.NewLeeum/js/ticket.js
+++ 03.NewLeeum/js/ticket.js
@@ -6,45 +6,26 @@ window.addEventListener("DOMContentLoaded", () => {
   console.log(`tickets`, tickets);
 
   let num = 0;
+  let isBlack = [false, false];
 
-  let isBlack0 = false;
-  let isBlack1 = false;
-
-  tickets[0].addEventListener("click", () => {
-    if (!isBlack0) {
-      tickets[0].classList.add("on");
-      textSelect[0].innerText = "취소";
-      num++;
-      isBlack0 = true;
-    } else {
-      tickets[0].classList.remove("on");
-      textSelect[0].innerText = "선택";
-      num--;
-      isBlack0 = false;
-    }
-    if (num !== 0) {
-      infoTicket.innerText = `${num}개의 전시를 선택했습니다.`;
-    } else {
-      infoTicket.innerText = "관람을 원하는 전시를 선택해주세요";
-    }
-  });
-
-  tickets[1].addEventListener("click", () => {
-    if (!isBlack1) {
-      tickets[1].classList.add("on");
-      textSelect[1].innerText = "취소";
-      num++;
-      isBlack1 = true;
-    } else {
-      tickets[1].classList.remove("on");
-      textSelect[1].innerText = "선택";
-      num--;
-      isBlack1 = false;
-    }
-    if (num !== 0) {
-      infoTicket.innerText = `${num}개의 전시를 선택했습니다.`;
-    } else {
-      infoTicket.innerText = "관람을 원하는 전시를 선택해주세요";
-    }
+  tickets.forEach((ele, idx) => {
+    tickets[idx].addEventListener("click", () => {
+      if (!isBlack[idx]) {
+        tickets[idx].classList.add("on");
+        textSelect[idx].innerText = "취소";
+        num++;
+        isBlack[idx] = true;
+      } else {
+        tickets[idx].classList.remove("on");
+        textSelect[idx].innerText = "선택";
+        num--;
+        isBlack[idx] = false;
+      }
+      if (num !== 0) {
+        infoTicket.innerText = `${num}개의 전시를 선택했습니다.`;
+      } else {
+        infoTicket.innerText = "관람을 원하는 전시를 선택해주세요";
+      }
+    });
   });
 });
