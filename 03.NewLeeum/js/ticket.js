e343fbc971c75bb980871917140d89f014e33f65
manemis <manemis@gmail.com>
2022-07-20 16:35:05 +0900

    made ticket page better

--- 03.NewLeeum/js/ticket.js
+++ 03.NewLeeum/js/ticket.js
@@ -1,21 +1,50 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const ticketSelectBtn = document.querySelectorAll(".selectBtn");
-  const ticket = document.querySelectorAll(".ticket");
+  const tickets = document.querySelectorAll(".ticket");
   const textSelect = document.querySelectorAll(".selectBtn span");
-  console.log(`textSelect:`, textSelect);
+  const infoTicket = document.querySelector(".infoTicket > span");
 
-  let isBlack = true;
-  ticketSelectBtn.forEach((ele, idx) => {
-    ele.addEventListener("click", () => {
-      if (isBlack) {
-        ticket[idx].classList.add("on");
-        textSelect[idx].innerText = "취소";
-        isBlack = false;
-      } else {
-        ticket[idx].classList.remove("on");
-        textSelect[idx].innerText = "선택";
-        isBlack = true;
-      }
-    });
+  console.log(`tickets`, tickets);
+
+  let num = 0;
+
+  let isBlack0 = false;
+  let isBlack1 = false;
+
+  tickets[0].addEventListener("click", () => {
+    if (!isBlack0) {
+      tickets[0].classList.add("on");
+      textSelect[0].innerText = "취소";
+      num++;
+      isBlack0 = true;
+    } else {
+      tickets[0].classList.remove("on");
+      textSelect[0].innerText = "선택";
+      num--;
+      isBlack0 = false;
+    }
+    if (num !== 0) {
+      infoTicket.innerText = `${num}개의 전시를 선택했습니다.`;
+    } else {
+      infoTicket.innerText = "관람을 원하는 전시를 선택해주세요";
+    }
+  });
+
+  tickets[1].addEventListener("click", () => {
+    if (!isBlack1) {
+      tickets[1].classList.add("on");
+      textSelect[1].innerText = "취소";
+      num++;
+      isBlack1 = true;
+    } else {
+      tickets[1].classList.remove("on");
+      textSelect[1].innerText = "선택";
+      num--;
+      isBlack1 = false;
+    }
+    if (num !== 0) {
+      infoTicket.innerText = `${num}개의 전시를 선택했습니다.`;
+    } else {
+      infoTicket.innerText = "관람을 원하는 전시를 선택해주세요";
+    }
   });
 });
